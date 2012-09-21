e14457b3550fdd2cfb733ae7a72ef9c5f7080c71
Heinz Riener <hriener@informatik.uni-bremen.de>
2012-09-21 15:09:47 +0200

    added two options (solver_executable and solver_arguments) to select particular command line solver

--- src/metaSMT/support/find_executable.hpp
+++ src/metaSMT/support/find_executable.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <unistd.h>
 
 namespace metaSMT {
   namespace support {
@@ -9,6 +10,14 @@ namespace metaSMT {
       char* env = getenv(envname.c_str());
       if(env) return env;
       return progname;
+    }
+
+    int execute(const std::string& file, const std::vector<std::string>& argv) {
+      std::vector<const char*> av;
+      for (std::vector<std::string>::const_iterator i = argv.begin(); i != argv.end(); ++i)
+        av.push_back(i->c_str());
+      av.push_back(static_cast<const char*>(0));
+      return execvp(file.c_str(), const_cast<char * const* >(&av[0]));
     }
   } /* support */
 } /* metaSMT */
