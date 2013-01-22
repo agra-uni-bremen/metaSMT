5b470e1682ba173c125fdca00e2dc07c895094b3
Heinz Riener <hriener@informatik.uni-bremen.de>
2013-01-22 13:58:52 +0100

    removed incomplete error handling from SMT2Parser

--- src/metaSMT/support/parser/SMT2Parser.hpp
+++ src/metaSMT/support/parser/SMT2Parser.hpp
@@ -1,262 +1,114 @@
-
 #pragma once
-
-#include <istream>
 #include <boost/spirit/include/support_utree.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/support_istream_iterator.hpp>
-#include <boost/spirit/home/support/info.hpp>
-#include <boost/spirit/include/support_line_pos_iterator.hpp>
+#include <istream>
 
-#include <boost/foreach.hpp>
-
-namespace metaSMT
-{
-  namespace smt2
-  {
+namespace metaSMT {
+  namespace smt2 {
     namespace spirit   = boost::spirit;
     namespace qi       = boost::spirit::qi;
     namespace phoenix  = boost::phoenix;
     namespace standard = boost::spirit::standard;
     namespace ascii    = boost::spirit::ascii;
 
-    using boost::spirit::utree;
-    using boost::spirit::utree_type;
-    using boost::spirit::info; 
+    namespace detail {
+      template<typename Iterator>
+      struct whitespace : qi::grammar< Iterator >  {
+        whitespace()
+          : whitespace::base_type(start) {
+          using standard::space;
+          using standard::char_;
+          using qi::eol;
 
-    namespace detail
-    {
-      bool is_list_node ( utree const& u )
-      {
-        return u.which() == utree_type::list_type; 
-      }
-
-    }
-
-    template<typename Out>
-      struct print_info
-      {
-        typedef boost::spirit::utf8_string string;
-
-        print_info ( Out& out ) : out ( out ), first( true ) {}
-
-        void element ( string const& tag, string const& value, int ) const
-        {
-          if (!first) 
-          {
-            out << ' ';
-            first = false;
-          }
-
-          if ( value == "" )
-            out << tag;
-          else
-            out << "\"" << value << '"';
+          start = space | ( ';' >> *(char_ - eol) >> eol );
         }
 
-        Out& out;
-        mutable bool first; 
+        qi::rule<Iterator> start;
+      }; // whitespace
 
-      }; 
+      template < typename Iterator, typename Evaluator >
+      struct parser : qi::grammar< Iterator, spirit::utree::list_type(), whitespace<Iterator> > {
+        parser( Evaluator &eval )
+          : parser::base_type(start)
+          , evaluate(eval)
+          , evaluator(eval) {
 
-    struct expected_component : std::exception
-    {
-      std::string msg;
+          start %= *( command );
 
-      expected_component ( std::string const& source, std::size_t line, info const& w)
-      {
-        using boost::spirit::basic_info_walker;
+          command %= '(' >> command_name >> *sexpr >> ')';
 
-        std::ostringstream oss;
-        oss << "(exception \"" << source << "\" ";
+          sexpr %= '(' >> *sexpr >> ')' | symbol | numeral;
 
-        if ( line == -1 )
-          oss << -1;
-        else
-          oss << line;
+          symbol %= qi::lexeme [ +(qi::char_ -end_of_word ) ];
 
-        oss << " '(expected_component (";
+          numeral %= qi::uint_;
 
-        print_info < std::ostringstream > pr ( oss );
-        basic_info_walker < print_info < std::ostringstream > > walker ( pr, w.tag, 0 );
+          command_name %= qi::lexeme [ +(qi::char_ - end_of_word ) ];
 
-        boost::apply_visitor ( walker, w.value );
+          end_of_word %= qi::space | qi::char_(")");
 
-        oss << ")))";
-
-        msg = oss.str();
-      }
-
-      virtual ~expected_component() throw() {}
-
-      virtual char const* what() const throw()
-      {
-        return msg.c_str(); 
-      }
-    }; 
-
-    template<typename Iterator>
-      struct error_handler
-      {
-        template<typename, typename, typename, typename >
-          struct result
-          {
-            typedef void type;
-          };
-
-        error_handler ( ) {}
-
-        void operator() (Iterator first, Iterator last, Iterator err_pos, info const& what ) const
-        {
-          using boost::spirit::get_line;
-          Iterator eol = err_pos;
-          std::size_t line = get_line (err_pos);
-        }
-      };
-
-    template<typename Iterator>
-      struct whitespace : qi::grammar < Iterator > 
-    {
-      qi::rule<Iterator> start;
-
-      whitespace() : whitespace::base_type ( start )
-      {
-        using standard::space;
-        using standard::char_;
-        using qi::eol;
-
-        start = space | ( ';' >> *(char_ - eol) >> eol ); 
-      }
-
-    };
-
-
-    template<typename Iterator, typename Evaluator, typename ErrorHandler = error_handler<Iterator> >
-    struct parser : qi::grammar < Iterator, utree::list_type(), whitespace<Iterator> >
-    {
-      qi::rule<Iterator, utree::list_type(), whitespace < Iterator > > start;
-      qi::rule<Iterator, utree(), whitespace < Iterator > > numeral, sexpr;
-      qi::rule<Iterator, boost::spirit::utf8_symbol_type(), whitespace < Iterator > > command_name;
-      qi::rule<Iterator, boost::spirit::utf8_string_type(), whitespace < Iterator > > symbol;
-      qi::rule<Iterator, utree::list_type(), whitespace < Iterator> > command;
-      qi::rule<Iterator > end_of_word;  
-
-      phoenix::function<ErrorHandler> const error;
-      phoenix::function<Evaluator> const evaluate;
-
-      parser ( Evaluator& evaluator ) : parser::base_type ( start ), evaluate ( evaluator ), error(ErrorHandler()), evaluator ( evaluator )
-      {
-        using standard::char_;
-        using qi::unused_type;
-        using qi::lexeme;
-        using qi::hex;
-        using qi::int_;
-        using qi::oct;
-        using qi::no_case;
-        using qi::real_parser;
-        using qi::uint_parser;
-        using qi::bool_parser;
-        using qi::on_error;
-        using qi::fail;
-        using qi::int_;
-        using qi::lit;
-        using qi::_val;
-        using qi::_1;
-        using qi::_2;
-        using qi::_3;
-        using qi::_4;
-        using ascii::digit;
-        using ascii::graph;
-        using ascii::alpha;
-        using qi::debug;
-
-        start %= *( command ); // [ evaluate ( _val ) ] );
-
-        command %= '(' >> command_name >> *sexpr >> ')';
-
-        sexpr %= '(' >> *sexpr >> ')' | symbol | numeral;
-
-        symbol %= lexeme [ +(qi::char_ -end_of_word ) ];
-
-        numeral %= qi::uint_;
-
-        command_name %= lexeme [ +(char_ - end_of_word ) ];
-
-        end_of_word %= qi::space | char_(")");
-        
-        start.name ( "start" );
-        command.name ( "command" ) ;
-        sexpr.name ("s-expr" );
-        symbol.name ( "symbol" );
-        numeral.name ( "numeral" );
-        command_name.name ( "command_name" );
+          start.name( "start" );
+          command.name( "command" ) ;
+          sexpr.name( "s-expr" );
+          symbol.name( "symbol" );
+          numeral.name( "numeral" );
+          command_name.name( "command_name" );
 
 #if 0
-        debug ( start);
-        debug ( command );
-        debug ( sexpr );
-        debug ( symbol );
-        debug ( numeral );
-        debug ( command_name ); 
+          qi::debug ( start);
+          qi::debug ( command );
+          qi::debug ( sexpr );
+          qi::debug ( symbol );
+          qi::debug ( numeral );
+          qi::debug ( command_name );
 #endif
-      }
-      
-      Evaluator& evaluator;
-    }; 
-
-    template<typename Evaluator>
-      class SMT2Parser
-      {
-        public:
-          SMT2Parser ( Evaluator& evaluator )
-          : evaluator ( evaluator )
-        {
         }
 
-          bool parse ( std::istream& instream, boost::spirit::utree::list_type& ast )
-          {
-            if ( !instream.good() )
-            {
-              return false; 
-            }
+        qi::rule<Iterator, spirit::utree::list_type(), whitespace< Iterator > > start;
+        qi::rule<Iterator, spirit::utree(), whitespace< Iterator > > numeral, sexpr;
+        qi::rule<Iterator, spirit::utf8_symbol_type(), whitespace< Iterator > > command_name;
+        qi::rule<Iterator, spirit::utf8_string_type(), whitespace< Iterator > > symbol;
+        qi::rule<Iterator, spirit::utree::list_type(), whitespace< Iterator> > command;
+        qi::rule<Iterator > end_of_word;
 
-            instream.unsetf ( std::ios::skipws );
+        phoenix::function<Evaluator> const evaluate;
+        Evaluator& evaluator;
+      }; // parser
+    } // detail
 
-            spirit::istream_iterator begin(instream);
-            spirit::istream_iterator end;
+    template < typename Evaluator >
+    class SMT2Parser {
+    public:
+      SMT2Parser( Evaluator &evaluator )
+        : evaluator(evaluator)
+      {}
 
-            parser<spirit::istream_iterator, Evaluator> p ( evaluator );
-            whitespace<spirit::istream_iterator> ws;
+      bool parse( std::istream &instream, spirit::utree::list_type &ast ) {
+        if ( !instream.good() ) {
+          return false;
+        }
 
-            bool r = phrase_parse ( begin, end, p, ws, ast );
+        instream.unsetf ( std::ios::skipws );
 
-            if ( !r )
-            {
-              std::cout << "Not r " << std::endl;
-            }
+        spirit::istream_iterator begin(instream);
+        spirit::istream_iterator end;
 
-            if ( begin != end )
-            {
-              std::cout << "begin != end" << std::endl;
-            }
+        detail::parser<spirit::istream_iterator, Evaluator> p( evaluator );
+        detail::whitespace<spirit::istream_iterator> ws;
+        bool r = phrase_parse( begin, end, p, ws, ast );
+        if ( r && begin == end ) {
+          return true;
+        }
+        else {
+          std::cerr << "ERROR: Parsing failed!" << std::endl;
+          std::cerr << "Stop at:" << std::string(begin, end) << std::endl;
+          return false;
+        }
+      }
 
-            if ( r && begin == end )
-            {
-              return true;
-            }
-            else
-            {
-              std::cerr << "Parsing failed." << std::endl;
-              std::cout << "Stop at:" << std::string ( begin, end ) << std::endl; 
-              return false; 
-            }
+    private:
+      Evaluator &evaluator;
+    }; // SMT2Parser
 
-            return r;
-
-          }
-
-        private:
-          Evaluator&                       evaluator;
-      };
-  }
-}
+  } // smt2
+} // metaSMT
