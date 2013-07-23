e977cf415c9e076dbd4991d08bff60b7af6ad54e
Heinz Riener <hriener@informatik.uni-bremen.de>
2013-07-23 10:51:49 +0200

    smt2Input_evaluator reads strings of unlimited length and does not wait for an command ``(exit)''

--- toolbox/smt2Input_evaluator/main.cpp
+++ toolbox/smt2Input_evaluator/main.cpp
@@ -11,33 +11,31 @@ using namespace metaSMT::evaluator;
 using namespace metaSMT::smt2;
 
 int main(int argc, char **argv) {
-  int const STREAM_SIZE = 65535;
   typedef UTreeEvaluator<ContextType> Evaluator;
-  char inputline[STREAM_SIZE];
   string line;
   stringstream *buf = new stringstream;
   ContextType ctx;
   Evaluator evaluator(ctx);
   SMT2Parser<Evaluator> parser(evaluator);
-  while(!cin.eof()){
-    cin.getline(inputline,STREAM_SIZE);
-    assert( !std::cin.fail() );
+  while( !cin.eof() ){
+    std::getline(std::cin, line);
     if ( std::cin.fail() ) {
-      std::cerr << "Error during input operation, e.g., stream size is too low" << '\n';
-      return -1;
+      // stream end without exit or read error
+      return 0;
     }
-    line = inputline;
+
     // std::cerr << line << '\n';
     *buf << line << endl;
     size_t found = line.find("(get-value");
-    if(line.compare("(check-sat)") == 0 || found != line.npos){
+    if (line == "(check-sat)" || found != line.npos) {
       boost::spirit::utree::list_type ast;
       parser.parse(*buf, ast);
       evaluator.evaluateInstance(ast);
       delete buf;
       buf = new stringstream;
     }
-    if(line.compare("(exit)") == 0){
+
+    if ( line == "(exit)" ) {
       break;
     }
   }
