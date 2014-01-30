1b10fc592157463ad5bf8c5d3e351587d3c23569
Heinz Riener <hriener@informatik.uni-bremen.de>
2014-01-30 17:03:31 +0100

    added support for solvers STP, SWORD, MiniSAT, PicoSAT, and CUDD to metaSMT-server

--- toolbox/metaSMT-server/Solver_Name_Map.hpp
+++ toolbox/metaSMT-server/Solver_Name_Map.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <boost/lexical_cast.hpp>
 #include <metaSMT/DirectSolver_Context.hpp>
 #include <metaSMT/API/Stack.hpp>
 #include <metaSMT/API/Group.hpp>
@@ -8,6 +9,10 @@
 #include <metaSMT/backend/PicoSAT.hpp>
 #include <metaSMT/backend/Z3_Backend.hpp>
 #include <metaSMT/backend/SMT2.hpp>
+#include <metaSMT/backend/STP.hpp>
+#include <metaSMT/backend/SWORD_Backend.hpp>
+#include <metaSMT/backend/MiniSAT.hpp>
+#include <metaSMT/backend/CUDD_Context.hpp>
 #include <boost/mpl/map.hpp>
 #include <boost/mpl/string.hpp>
 #include <boost/mpl/for_each.hpp>
@@ -15,16 +20,36 @@
 namespace metaSMT {
   typedef boost::mpl::map<
     mpl::pair<
-      mpl::string<'Z','3'>
-    , DirectSolver_Context< IgnoreSymbolTable< solver::Z3_Backend> >
+      string_concat< mpl::string<'B','o','o','l'>, mpl::string<'e','c','t','o','r'> >::type
+    , DirectSolver_Context< IgnoreSymbolTable< Stack<solver::Boolector> > >
     >
   , mpl::pair<
-      string_concat< mpl::string<'B','o','o','l'>, mpl::string<'e','c','t','o','r'> >::type
-      , DirectSolver_Context< IgnoreSymbolTable< Stack<solver::Boolector> > >
+      mpl::string<'C', 'U', 'D', 'D'>
+    , DirectSolver_Context< IgnoreSymbolTable< Stack< Group < solver::CUDD_Context > > > >
+    >
+  , mpl::pair<
+      mpl::string<'M','i','n','i', 'S', 'A', 'T'>
+    , DirectSolver_Context< IgnoreSymbolTable< Stack< Group < BitBlast < SAT_Clause < solver::MiniSAT > > > > > >
+    >
+  , mpl::pair<
+      mpl::string<'P','i','c','o', 'S', 'A', 'T'>
+    , DirectSolver_Context< IgnoreSymbolTable< Stack< Group<BitBlast < SAT_Clause < solver::PicoSAT > > > > > >
     >
   , mpl::pair<
       mpl::string<'S','M','T','2'>
     , DirectSolver_Context< solver::SMT2 >
+    >
+  , mpl::pair<
+      mpl::string<'S','T','P'>
+    , DirectSolver_Context< IgnoreSymbolTable<Stack<solver::STP> > >
+    >
+  , mpl::pair<
+      mpl::string<'S','W','O','R', 'D'>
+    , DirectSolver_Context< IgnoreSymbolTable< Stack<solver::SWORD_Backend> > >
+    >
+  , mpl::pair<
+      mpl::string<'Z','3'>
+    , DirectSolver_Context< IgnoreSymbolTable< solver::Z3_Backend> >
     >
   > Solver_Name_Map;
 
