be176cc632777a4a353b6cef2ec30709978d5194
Finn Haedicke <finn@informatik.uni-bremen.de>
2015-04-16 09:50:49 +0200

    fixed warnings in library_rpath.cmake
    Only recurse if the current library is a TARGET, not when it is just
    non-empty.
    Also remove duplicates from the collected rpath.

--- cmake/library_rpath.cmake
+++ cmake/library_rpath.cmake
@@ -4,20 +4,31 @@
 # if the library is a target use its location and recurse on
 # its dependencies.
 
-macro(add_dependent_libraries)
+macro( _recursive_collect_rpath )
   foreach( lib ${ARGN})
     if(EXISTS ${lib})
       get_filename_component(libdir ${lib} PATH)
-      list(APPEND CMAKE_INSTALL_RPATH ${libdir})
-    elseif(lib)
+      list(APPEND _rpath ${libdir})
+    elseif(TARGET ${lib})
       get_target_property(libpath ${lib} LOCATION)
       get_filename_component(libdir ${libpath} PATH)
-      list(APPEND CMAKE_INSTALL_RPATH ${libdir})
+      list(APPEND _rpath ${libdir})
       get_target_property(dependent ${lib} IMPORTED_LINK_INTERFACE_LIBRARIES)
-      add_dependent_libraries(${dependent})
+      _recursive_collect_rpath(${dependent})
       get_target_property(dependent ${lib} IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE)
-      add_dependent_libraries(${dependent})
+      _recursive_collect_rpath(${dependent})
     endif()
-  endforeach(lib)
-endmacro(add_dependent_libraries)
+  endforeach()
+endmacro( )
+
+function(add_dependent_libraries)
+  set( _rpath "")
+
+  _recursive_collect_rpath( ${ARGN} )
+
+  list( SORT _rpath )
+  list( REMOVE_DUPLICATES _rpath )
+
+  set( CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_RPATH} ${_rpath} PARENT_SCOPE)
+endfunction()
 
