05c9f98eeb38a60ca34521f6fd2b5bd88b1ec681
Hoang M. Le <hle@informatik.uni-bremen.de>
2016-01-25 14:34:58 +0100

    workaround for the boost::atomic issue

--- src/lib/_var_id.cpp
+++ src/lib/_var_id.cpp
@@ -1,14 +1,24 @@
 #include "../metaSMT/impl/_var_id.hpp"
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 105500
 
 #include <boost/atomic.hpp>
+unsigned metaSMT::impl::new_var_id()
+{
+  static boost::atomic_uint _id ( 0u );
+  ++_id;
+  return _id;
+} 
 
-namespace metaSMT {
-  namespace impl {
-      unsigned new_var_id(  )
-      {
-        static boost::atomic_uint _id ( 0u );
-        ++_id;
-        return _id;
-      } 
-  } /* impl */
-} /* metaSMT */
+#else
+
+unsigned metaSMT::impl::new_var_id()
+{
+  static unsigned _id ( 0u );
+  ++_id;
+  return _id;
+} 
+
+#endif
+
